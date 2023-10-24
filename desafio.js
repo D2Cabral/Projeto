b8c8bde98691af40e295b2b0c463590870d0aacb
D2Cabral <diegomirks@icloud.com>
2023-10-24 16:41:29 -0300

    Fim

---
+++ desafio.js
@@ -0,0 +1,27 @@
+let heroi = ["Março pilame", 4000];
+
+if (heroi[1] < 1000) {
+    console.log("O Herói de nome " + heroi[0]+ " está no nível de"  + " Ferro")
+
+}else if (heroi[1] <= 2000){
+
+    console.log("O Herói de nome " + heroi[0]+ " está no nível de"  + " Bronze")
+} else if (heroi[1]  <= 5000){
+
+    console.log("O Herói de nome " + heroi[0]+ " está no nível de"  + " Prata")
+}  else if (heroi[1]  <= 7000){
+
+    console.log("O Herói de nome " + heroi[0]+ " está no nível de"  + " Ouro")
+} else if (heroi[1]  <= 8000){
+
+    console.log("O Herói de nome " + heroi[0]+ " está no nível de"  + " Platina")
+} else if (heroi[1]  <= 9000){
+
+    console.log("O Herói de nome " + heroi[0]+ " está no nível de"  + " Ascendente")
+} else if (heroi[1] <= 10000){
+
+    console.log("O Herói de nome " + heroi[0]+ " está no nível de"  + " Imortal")
+} else if (heroi[1] >= 10001){
+
+    console.log("O Herói de nome " + heroi[0]+ " está no nível de"  + " Radiante")
+} 
